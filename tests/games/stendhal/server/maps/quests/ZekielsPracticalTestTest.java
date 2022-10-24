dc2ce1f82c1bd3777c400fb590b19a62124dffa6
Oliver Quinn <oliver.quinn-2@student.manchester.ac.uk>
2022-10-24 15:16:04 +0100

    Fixed Assertion Argument

--- tests/games/stendhal/server/maps/quests/ZekielsPracticalTestTest.java
+++ tests/games/stendhal/server/maps/quests/ZekielsPracticalTestTest.java
@@ -110,11 +110,11 @@ public class ZekielsPracticalTestTest {
 		 */// FROM UseActionTest should put candle on floor
 		
 		new EquipItemAction("candle",1);
-		assertEquals(true,new PlayerHasItemWithHimCondition("candle"));
+		assertEquals("player has item <1 candle>",new PlayerHasItemWithHimCondition("candle"));
 		
 		new DropItemAction("candle",1);
 		
-		assertEquals(true,new PlayerHasItemWithHimCondition("candle"));
+		assertEquals("player has item <1 candle>",new PlayerHasItemWithHimCondition("candle"));
 		
 		//Teleport to basement
 		/*
