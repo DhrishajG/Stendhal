5f84b9dfa85ae7029934eb3dc8310a64897a1ed2
Oliver Quinn <oliver.quinn-2@student.manchester.ac.uk>
2022-11-25 13:11:47 +0000

    Completed base functionality
    Co-authored-by: Dhrishaj Garg <dhrishaj.garg@student.manchester.ac.uk>

--- src/games/stendhal/server/entity/mapstuff/cart/Cart.java
+++ src/games/stendhal/server/entity/mapstuff/cart/Cart.java
@@ -1,33 +1,65 @@
 package games.stendhal.server.entity.mapstuff.cart;
 
-import java.util.Iterator;
-import games.stendhal.common.grammar.Grammar;
+//import java.util.Iterator;
+//import games.stendhal.common.grammar.Grammar;
 import games.stendhal.server.core.events.UseListener;
-import games.stendhal.server.entity.Entity;
-import games.stendhal.server.entity.PassiveEntity;
+//import games.stendhal.server.entity.Entity;
+//import games.stendhal.server.entity.PassiveEntity;
 import games.stendhal.server.entity.RPEntity;
-import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.entity.slot.ChestSlot;
-import marauroa.common.game.Definition.Type;
+import games.stendhal.server.entity.mapstuff.block.Block;
+import games.stendhal.server.entity.mapstuff.chest.Chest;
+//import games.stendhal.server.entity.player.Player;
+//import games.stendhal.server.entity.slot.ChestSlot;
+//import marauroa.common.game.Definition.Type;
+//import marauroa.common.game.RPClass;
+//import marauroa.common.game.RPObject;
+//import marauroa.common.game.RPSlot;
 import marauroa.common.game.RPClass;
-import marauroa.common.game.RPObject;
-import marauroa.common.game.RPSlot;
+import marauroa.common.game.Definition.Type;
 
-public class Cart extends Entity{
+public class Cart extends Block implements UseListener{
 	private static final String CART_RPCLASS_NAME = "cart";
+	private static final String Z_ORDER = "z";
+
+	
+	private Chest cartChest;
 	
 	public Cart() {
+		super(true,"hay_cart"); //Init pushable block with hay cart sprite [sprite untested]
+		cartChest = new Chest();
 		setRPClass(CART_RPCLASS_NAME);
 		put("type", CART_RPCLASS_NAME);
 	}
 	
-	public static void generateRPClass() {
-//		if (!RPClass.hasRPClass(CHEST_RPCLASS_NAME)) {
-//			final RPClass chest = new RPClass(CHEST_RPCLASS_NAME);
-//			chest.isA("entity");
-//			chest.addAttribute("open", Type.FLAG);
-//			chest.addRPSlot("content", 30);
-//		}
+	public Cart(boolean multiPush) {
+		super(multiPush);
 	}
+	
+	public static void generateRPClass() {
+		RPClass clazz = new RPClass("cart");
+		clazz.isA("area");
+		// z order to control client side drawing
+		clazz.addAttribute(Z_ORDER, Type.INT);
+		clazz.addAttribute("class", Type.STRING);
+		clazz.addAttribute("shape", Type.STRING);
+	}
+	
+	//UseListener
+	@Override
+	public boolean onUsed(final RPEntity user) {
+		return cartChest.onUsed(user);
+	}
+	
+	public boolean isOpen() {
+		return cartChest.isOpen();
+	}
+	
+	public void open() {
+		cartChest.open();
+	}
+
+	public void close() {
+		cartChest.close();
+		}
 	
 }
