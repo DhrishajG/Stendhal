1d3e0520235bd0e609a811245d3051c4c506114b
Dhrishaj Garg <dhrishaj.garg@student.manchester.ac.uk>
2022-11-25 13:08:46 +0000

    Completed test file.
    Co-authored-by: Oliver Quinn <oliver.quinn-2@student.manchester.ac.uk>

--- tests/games/stendhal/server/entity/mapstuff/cart/CartTest.java
+++ tests/games/stendhal/server/entity/mapstuff/cart/CartTest.java
@@ -12,26 +12,42 @@
  ***************************************************************************/
 package games.stendhal.server.entity.mapstuff.cart;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.is;
+//import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import games.stendhal.server.actions.move.PushAction;
+import games.stendhal.common.Direction;
+//import games.stendhal.server.actions.move.PushAction;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.Entity;
-import games.stendhal.server.entity.PassiveEntity;
-import games.stendhal.server.entity.RPEntity;
-import games.stendhal.server.entity.item.Corpse;
-import games.stendhal.server.entity.mapstuff.chest.Chest;
+//import games.stendhal.server.entity.PassiveEntity;
+//import games.stendhal.server.entity.RPEntity;
+//import games.stendhal.server.entity.item.Corpse;
+//import games.stendhal.server.entity.mapstuff.cart.Cart;
+//import games.stendhal.server.entity.mapstuff.chest.Chest;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.game.RPClass;
-import marauroa.common.game.SlotIsFullException;
+//import marauroa.common.game.SlotIsFullException;
 import utilities.PlayerTestHelper;
+import utilities.RPClass.BlockTestHelper;
 
 public class CartTest{
+	
+
+	@BeforeClass
+	public static void beforeClass() {
+		BlockTestHelper.generateRPClasses();
+		PlayerTestHelper.generatePlayerRPClasses();
+	    MockStendlRPWorld.get();
+	}
 	@Before
 	public void setUp() throws Exception {
 		if (!RPClass.hasRPClass("entity")) {
@@ -49,15 +65,108 @@ public class CartTest{
 	}
 	//Player should be able to push cart
 	@Test
-	public final void pushable() {
-		Cart cart = new Cart();
-		Player player = PlayerTestHelper.createPlayer("bob");
-		AssertEquals(player.canPush(cart),true);
+	public void testPush() {
+		Cart b = new Cart(true);
+		b.setPosition(0, 0);
+		StendhalRPZone z = new StendhalRPZone("test", 10, 10);
+		Player p = PlayerTestHelper.createPlayer("pusher");
+		z.add(b);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.push(p, Direction.RIGHT);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(1)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.push(p, Direction.LEFT);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.push(p, Direction.DOWN);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(1)));
+
+		b.push(p, Direction.UP);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+	}
+
+	@Test
+	public void testMultiPush() {
+		Cart b = new Cart(false);
+		b.setPosition(0, 0);
+		StendhalRPZone z = new StendhalRPZone("test", 10, 10);
+		Player p = PlayerTestHelper.createPlayer("pusher");
+
+		z.add(b);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.push(p, Direction.RIGHT);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(1)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.push(p, Direction.LEFT);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(1)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(0)));
+
+		b.reset();
+		//after a reset the cart does not count as pushed
+
+		b.push(p, Direction.DOWN);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(1)));
+
+		// but a second push should be prevented
+		b.push(p, Direction.UP);
+		assertThat(Integer.valueOf(b.getX()), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getY()), is(Integer.valueOf(1)));
+	}
+
+
+	@Test
+	public void testCoordinatesAfterPush() {
+		Cart b = new Cart(true);
+		b.setPosition(0, 0);
+		assertThat(Integer.valueOf(b.getXAfterPush(Direction.UP)), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getYAfterPush(Direction.UP)), is(Integer.valueOf(-1)));
+
+		assertThat(Integer.valueOf(b.getXAfterPush(Direction.DOWN)), is(Integer.valueOf(0)));
+		assertThat(Integer.valueOf(b.getYAfterPush(Direction.DOWN)), is(Integer.valueOf(1)));
+
+		assertThat(Integer.valueOf(b.getXAfterPush(Direction.LEFT)), is(Integer.valueOf(-1)));
+		assertThat(Integer.valueOf(b.getYAfterPush(Direction.LEFT)), is(Integer.valueOf(0)));
+
+		assertThat(Integer.valueOf(b.getXAfterPush(Direction.RIGHT)), is(Integer.valueOf(1)));
+		assertThat(Integer.valueOf(b.getYAfterPush(Direction.RIGHT)), is(Integer.valueOf(0)));
+	}
+
+	@Test
+	public void testCollisionOnPush() throws Exception {
+		Cart b1 = new Cart(true);
+		b1.setPosition(0, 0);
+		StendhalRPZone z = new StendhalRPZone("test", 10, 10);
+		Player p = PlayerTestHelper.createPlayer("pusher");
+		z.add(b1, false);
+
+		// one successful push
+		b1.push(p, Direction.RIGHT);
+		assertThat(Integer.valueOf(b1.getX()), is(Integer.valueOf(1)));
+
+		// now we add an obstacle right of b1
+		Cart b2 = new Cart(true);
+		b2.setPosition(02, 0);
+		z.add(b2, false);
+
+		// push should not be executed now and stay at the former place
+		b1.push(p, Direction.RIGHT);
+		assertThat(Integer.valueOf(b1.getX()), is(Integer.valueOf(1)));
 	}
 	
 	@Test
 	public final void testOpen() {
 		final Cart cart = new Cart();
+
 		assertFalse(cart.isOpen());
 		cart.open();
 
