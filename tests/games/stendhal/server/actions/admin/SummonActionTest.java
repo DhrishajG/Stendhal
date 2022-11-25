21456f4726d315f0f85c915171a6a8812e5186ec
Shemaine Vij <shemaine.vij@student.manchester.ac.uk>
2022-11-25 09:54:54 +0000

    test1 for stendhal

--- tests/games/stendhal/server/actions/admin/SummonActionTest.java
+++ tests/games/stendhal/server/actions/admin/SummonActionTest.java
@@ -152,6 +152,26 @@ public class SummonActionTest {
 		final Item item = (Item) zone.getEntityAt(0, 0);
 		assertEquals("fishing_rod", item.get("subclass"));
 	}
+	
+	
+	public final void testSummonBlackHelmet() {
+		final Player pl = PlayerTestHelper.createPlayer("hugo");
+
+		MockStendhalRPRuleProcessor.get().addPlayer(pl);
+
+		zone.add(pl);
+		pl.setPosition(1, 1);
+		pl.put("adminlevel", 5000);
+		final RPAction action = new RPAction();
+		action.put("type", "summon");
+		action.put("creature", "black helmet");
+		action.put("x", 0);
+		action.put("y", 0);
+		CommandCenter.execute(pl, action);
+		assertEquals(1, pl.getID().getObjectID());
+		final Item item = (Item) zone.getEntityAt(0, 0);
+		assertEquals("black_helmet", item.get("subclass"));
+	}
 
 	/**
 	 * Tests for avoidNFE.
